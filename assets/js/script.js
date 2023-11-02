da4ddce0e3965cdcab09efca71843bf3104dc740
ArnaudDarre <arnaudarre@gmail.com>
2023-11-02 10:00:52 +0000

    Add cookie bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
 
-// —————————————————————————————————————————————————————————————————
-// back to top
-// cookie bar accept
-// —————————————————————————————————————————————————————————————————
-
-// —————————————————————————————————————————————————————————————————
-// back to top
-// —————————————————————————————————————————————————————————————————
-
 var btn = $('.button--scroll-top');
 
 $(window).scroll(function() {
@@ -24,32 +15,5 @@ btn.on('click', function(e) {
 	$('html, body').animate({scrollTop:0}, '400');
 });
 
-// —————————————————————————————————————————————————————————————————
-// cookie bar accept
-// —————————————————————————————————————————————————————————————————
-
-var cookieKey = "__arnaud_darre_portfolio_cookie_banner"
-var cookieClose = function() {
-	if (typeof localStorage !== 'undefined') {
-		localStorage[cookieKey] = "true"
-	}
-}
-
-var displayCookieBar = function() {
-	if (typeof localStorage !== 'undefined') {
-		if (!localStorage[cookieKey]) {
-			$(".cookie").css("display", "flex")
-		}
-	}
-}
-
-$(".cookie__button").on("click", cookieClose)
-displayCookieBar()
-
-$(".cookie__button").click(function(){
-	$(".cookie").addClass("js-cookie-accepted");
-})
-
-// —————————————————————————————————————————————————————————————————
 
 });
