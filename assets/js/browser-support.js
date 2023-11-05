0f724b06ee4c215f48ba3b48ddb90dd3c610f708
ArnaudDarre <arnaudarre@gmail.com>
2023-11-05 09:46:17 +0000

    Test compatibility alert on IE

--- assets/js/browser-support.js
+++ assets/js/browser-support.js
@@ -1,5 +1,45 @@
-var supportsCssVars = function () {
-  var e, t = document.createElement("style");
-  return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e
-};
-supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
+function isUnsupported(browser) {
+  return (browser.name === "MSIE" || browser.name === "IE")
+}
+var isUnsupported = this.isUnsupported(this.get_browser());
+// Uncomment to simulate being in an IE browser.
+// var isUnsupported = true;
+
+this.listen("load", window, function () {
+  if (isUnsupported) {
+    document.querySelector(".browserWarning").style.display = 'flex';
+    document.body.classList.add("js-freeze");
+  }
+});
+
+function listen(evnt, elem, func) {
+  if (elem.addEventListener)  // W3C DOM
+    elem.addEventListener(evnt, func, false);
+  else if (elem.attachEvent) { // IE DOM
+    var r = elem.attachEvent("on" + evnt, func);
+    return r;
+  }
+  else window.alert('Error: unsupported browser!');
+}
+
+function get_browser() {
+  var ua = navigator.userAgent, tem, M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return { name: 'IE', version: (tem[1] || '') };
+  }
+  if (M[1] === 'Chrome') {
+    tem = ua.match(/\bOPR\/(\d+)/)
+    if (tem != null) { return { name: 'Opera', version: tem[1] }; }
+  }
+  if (window.navigator.userAgent.indexOf("Edge") > -1) {
+    tem = ua.match(/\Edge\/(\d+)/)
+    if (tem != null) { return { name: 'Edge', version: tem[1] }; }
+  }
+  M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i)) != null) { M.splice(1, 1, tem[1]); }
+  return {
+    name: M[0],
+    version: +M[1]
+  };
+}
