f95a53f536195799ab888fa2881fbaf0b3224ad7
ArnaudDarre <arnaudarre@gmail.com>
2023-11-05 09:54:48 +0000

    Simplify browser support test

--- assets/js/browser-support.js
+++ assets/js/browser-support.js
@@ -1,9 +1,4 @@
-var supportsCssVars = function () {
-  var e, t = document.createElement("style");
-  return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e
-};
-
-if (!supportsCssVars()) {
-  document.querySelector(".browserWarning").style.display = 'flex';
+if (!window.CSS || !window.CSS.supports("color", "var(--fake-var)")) {
+  document.querySelector(".browserWarning").style.display = "flex";
   document.body.classList.add("js-freeze");
 }
