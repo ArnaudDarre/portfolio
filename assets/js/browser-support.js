ef53614109372ac3c1430c155bbe5c9d154fb3fb
ArnaudDarre <arnaudarre@gmail.com>
2023-11-05 09:32:48 +0000

    Test notice for older browsers

--- assets/js/browser-support.js
+++ assets/js/browser-support.js
@@ -2,8 +2,4 @@ var supportsCssVars = function () {
   var e, t = document.createElement("style");
   return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e
 };
-
-if (!supportsCssVars()) {
-  document.querySelector(".browserWarning").style.display = 'flex';
-  document.body.classList.add('js-freeze');
-}
+supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
