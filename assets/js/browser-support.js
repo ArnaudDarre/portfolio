37c9307e3bafcdf641739e9dbcba5a96ed351332
ArnaudDarre <arnaudarre@gmail.com>
2023-11-05 09:58:31 +0000

    Browser test support for IE 9 and lower

--- assets/js/browser-support.js
+++ assets/js/browser-support.js
@@ -1,9 +1,5 @@
-var supportsCssVars = function () {
-  var e, t = document.createElement("style");
-  return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e
-};
-
-if (!supportsCssVars()) {
-  document.querySelector(".browserWarning").style.display = 'flex';
+var style = document.createElement('div').style;
+if (!('setProperty' in style)) {
+  document.querySelector(".browserWarning").style.display = "flex";
   document.body.classList.add("js-freeze");
 }
